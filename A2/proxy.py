8aa89afaf4d664f732ce594f5b3be92ad5457ebe
ctsteele <ctsteele@ucalgary.ca>
2017-02-06 21:24:19 -0700

    framework for msg parsing

--- A2/proxy.py
+++ A2/proxy.py
@@ -2,10 +2,34 @@ import socket
 import sys
 import datetime
 
+logOption = ""
+allLogOptions = ["","-raw","-strip","-hex","-auto"]
+N = 0
+
+#only call if a logOption is selected besides ""
+def logMsg(msg, direction):
+	if (logOption == "-raw"):
+		#log in raw format
+		print("")
+	elif (logOption == "-strip"):
+		#log after replacing non-printable characters with "."
+		print("")
+	elif (logOption == "-hex"):
+		#logged in hexdump fashion
+		print("")
+	else:
+		#log in N byte chunks WILL NEED TO BE FOUND BEFORE
+		
+		#write message to file
+		#and print the message
+		print(direction + message)
+
 #main function to run the program
 def main():
+	global logOption
+	#collect command line arguments
 	if (len(sys.argv) == 5):
-		logOptions = sys.argv[1]
+		logOption = sys.argv[1]
 		srcPort = sys.argv[2]
 		server = sys.argv[3]
 		dstPort = sys.argv[4]
@@ -15,6 +39,14 @@ def main():
 		dstPort = sys.argv[3]
 	else:
 		print("command is:\n\t'python proxy.py [logOptions] srcPort server dstPort'")
+		quit()
+		
+	#extract N if -autoN is selected
+	if ("-auto" in logOption):
+		N = int(logOption[5:])
+	#else if it's not a valid logOption, quit the program
+	elif (logOption not in allLogOptions):
+		print("please ensure a valid logOption is entered")
 		quit()
 
 	sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
